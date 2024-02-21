5298f800b37322088178f13bffc7147b33276cda
TOBENTOch <info@tobento.ch>
2024-02-21 17:18:34 +0100

    type and desc added

--- src/Migration/Queue.php
+++ src/Migration/Queue.php
@@ -67,6 +67,8 @@ class Queue implements MigrationInterface
         return new Actions(
             new FilesCopy(
                 files: $this->files,
+                type: 'config',
+                description: 'Queue config file.',
             ),
         );
     }
@@ -79,7 +81,11 @@ class Queue implements MigrationInterface
     public function uninstall(): ActionsInterface
     {
         return new Actions(
-            new FilesDelete($this->files),
+            new FilesDelete(
+                files: $this->files,
+                type: 'config',
+                description: 'Queue config file.',
+            ),
         );
     }
 }
